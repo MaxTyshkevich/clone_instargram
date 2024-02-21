6ef3a33b388a5ac286ca0f927725befc48966819
Max Tyshkevich <2842778@gmail.com>
2024-02-21 18:44:24 +0300

    feat: skeleton singlePost

--- components/post/Skeletons.tsx
+++ components/post/Skeletons.tsx
@@ -31,7 +31,7 @@ const UserAvatarSkeleton = () => (
     <Skeleton className="h-12 w-12 rounded-full" />
     <div className="space-y-2">
       <Skeleton className="h-4 w-[250px]" />
-      <Skeleton className="h-4 w-[250px]" />
+      <Skeleton className="h-4 w-[200px]" />
     </div>
   </div>
 );
@@ -40,14 +40,22 @@ export const SinglePostSkeleton = () => {
   return (
     <Card className="max-w-3xl lg:max-w-4xl mx-auto hidden md:flex">
       <div className="relative overflow-hidden h-[450px] max-w-sm lg:max-w-lg w-full">
-        <Skeleton className="h-full w-full"></Skeleton>
+        <Skeleton className="h-full w-full" />
       </div>
-      <div></div>
-      <div>
-        <UserAvatarSkeleton />
-        <UserAvatarSkeleton />
-        <UserAvatarSkeleton />
-        <UserAvatarSkeleton />
+      <div className="flex max-w-sm flex-col flex-1">
+        <div className="flex items-center justify-between border-b px-5 py-3">
+          <Skeleton className="h-12 w-12 rounded-full" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+          </div>
+        </div>
+
+        <div className="px-5 space-y-3 mt-8">
+          <UserAvatarSkeleton />
+          <UserAvatarSkeleton />
+          <UserAvatarSkeleton />
+          <UserAvatarSkeleton />
+        </div>
       </div>
     </Card>
   );
